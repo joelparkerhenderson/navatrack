57640d25432a19c313beb93dd6469e87eb86cfde
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-10-20 18:57:29 +0100

    Fix USV parser for blank fields

--- lib/navatrack/usv.ex
+++ lib/navatrack/usv.ex
@@ -1,14 +1,17 @@
 defmodule USV do
-  def read!(path, drop = 0) do
-    File.read!(path)
-    |> String.splitter("␞")
-    |> Enum.drop(drop)
-    |> Stream.map(fn record ->
+
+  def read!(path, drop \\ 0) do
+    path
+    |> File.read!()
+    |> String.split("␞") # split on record separator
+    |> Enum.drop(-1) # drop last element which is empty
+    |> Enum.drop(drop) # drop any header records
+    |> Enum.map(fn record ->
       record
-      |> String.splitter("␟")
-      |> Enum.drop(-1)
-      |> Enum.to_list()
-      |> Enum.map(&String.trim/1)
+      |> String.split("␟") # split on unit separator
+      |> Enum.drop(-1) # drop last element which is empty
+      |> Enum.map(&String.trim/1) # clean up whitespace
     end)
   end
+
 end
