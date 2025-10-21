2477834f4fabd8d92c8f3f479897f1cfd2676833
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-10-21 18:50:09 +0100

    Add access control resources

---
+++ lib/navatrack/accounts/access_assignment.ex
@@ -0,0 +1,70 @@
+defmodule Navatrack.Accounts.AccessAssignment do
+  use Ash.Resource,
+    otp_app: :navatrack,
+    domain: Navatrack.Accounts,
+    data_layer: AshPostgres.DataLayer
+
+  def snake_case_singular(), do: "access_assignment"
+  def snake_case_plural(), do: "access_assignments"
+  def title_case_singular(), do: "Access Assignment"
+  def title_case_plural(), do: "Access Assignments"
+
+  postgres do
+    table "access_assignments"
+    repo Navatrack.Repo
+  end
+
+  actions do
+    defaults [:read, :destroy, :create, :update]
+    default_accept [
+      ### meta
+      :created_at,
+      :updated_at,
+      :deleted_at,
+      :locale_code,
+      :parent_id,
+      :parent_order,
+      ### relationships
+      :user_id,
+      :access_attribute_id
+    ]
+  end
+
+  attributes do
+    ### meta
+    uuid_primary_key :id
+    attribute :created_at, :utc_datetime_usec
+    attribute :updated_at, :utc_datetime_usec
+    attribute :deleted_at, :utc_datetime_usec
+    attribute :locale_code, :string
+    attribute :parent_id, :uuid
+    attribute :parent_order, :integer
+    ### relationships
+    attribute :user_id, :uuid
+    attribute :access_attribute_id, :uuid
+  end
+
+  relationships do
+    belongs_to :user, Navatrack.Accounts.User
+    belongs_to :access_attribute, Navatrack.Accounts.AccessAttribute
+  end
+
+  def fake(map \\ %{}) do
+    Map.merge(
+      %{
+        ### meta
+        locale_code: "en-US",
+        ### card
+        name: "my-name",
+        sign: "○",
+        status: "my-status",
+        tagging: "my-tagging",
+        note: "my-note",
+        user_id: nil,
+        access_attribute_id: nil,
+      },
+      map
+    )
+  end
+
+end
---
+++ lib/navatrack/accounts/access_operation.ex
@@ -0,0 +1,73 @@
+defmodule Navatrack.Accounts.AccessOperation do
+  use Ash.Resource,
+    otp_app: :navatrack,
+    domain: Navatrack.Accounts,
+    data_layer: AshPostgres.DataLayer
+
+  def snake_case_singular(), do: "access_operation"
+  def snake_case_plural(), do: "access_operations"
+  def title_case_singular(), do: "Access Operation"
+  def title_case_plural(), do: "Access Operations"
+
+  postgres do
+    table "access_operations"
+    repo Navatrack.Repo
+  end
+
+  actions do
+    defaults [:read, :destroy, :create, :update]
+    default_accept [
+      ### meta
+      :created_at,
+      :updated_at,
+      :deleted_at,
+      :locale_code,
+      :parent_id,
+      :parent_order,
+      ### card
+      :name,
+      :sign,
+      :status,
+      :tagging,
+      :note,
+    ]
+  end
+
+  attributes do
+    ### meta
+    uuid_primary_key :id
+    attribute :created_at, :utc_datetime_usec
+    attribute :updated_at, :utc_datetime_usec
+    attribute :deleted_at, :utc_datetime_usec
+    attribute :locale_code, :string
+    attribute :parent_id, :uuid
+    attribute :parent_order, :integer
+    ### card
+    attribute :name, :string
+    attribute :sign, :string
+    attribute :status, :string
+    attribute :tagging, :string
+    attribute :note, :string
+  end
+
+  relationships do
+    has_many :access_permissions, Navatrack.Accounts.AccessPermission
+  end
+
+  def fake(map \\ %{}) do
+    Map.merge(
+      %{
+        ### meta
+        locale_code: "en-US",
+        ### card
+        name: "my-name",
+        sign: "○",
+        status: "my-status",
+        tagging: "my-tagging",
+        note: "my-note"
+      },
+      map
+    )
+  end
+
+end
